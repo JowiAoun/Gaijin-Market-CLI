57e96a80a1f2c7df58948898120fc6b8196b4bcf
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-10-30 13:12:50 -0400

    get_open_orders and cancel_order

--- src/functions.py
+++ src/functions.py
@@ -31,6 +31,7 @@ DB_CURSOR = DB.cursor()
 
 
 # --- Functions
+# General functions
 def get_balance() -> float:
     """
     Gets the balance of the user.
@@ -43,15 +44,76 @@ def get_balance() -> float:
     conn.request("GET", "/GetBalance", '', headers)
     res = conn.getresponse()
     data = json.loads(res.read())
-    
     conn.close()
-
+    
     # Check if status is good
     if (data["status"] != "OK"):
         print(f"ERROR: could not get balance.\nStatus: {data['status']}")
         return -1
     else:
         return float(data["balance"])/10000
+
+def get_open_orders() -> list[tuple]:
+    """
+    Gets current open orders. Returns a list of tuples with data organized like so:
+    (transact_id, order_id, pair_id, hash_name, type, price, amount, timestamp).
+    """
+
+    conn = client.HTTPSConnection("market-proxy.gaijin.net")
+    payload = f"action=cln_get_user_open_orders&token={GM_TOKEN}&appid=1165"
+    headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    conn.request("POST", "/web", payload, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read())
+    conn.close()
+
+    open_orders = []
+
+    if (data['success'] != True):
+        print("ERROR: Could not make request for open orders.")
+        return []
+    
+    for item in data['response']:
+        try:
+            open_orders.append((int(item['txId']), int(item['id']), int(item['pairId']), item['market'], item['type'], round(int(item['localPrice'])/10000, 2), int(item['amount']), item['time']))
+        except:
+            print(f"ERROR: Could not get item in open orders with ID: {item['id']}")
+    
+    return open_orders
+
+def cancel_order(transact_id: int, order_id: int, pair_id: int) -> bool:
+    """
+    Cancels an open order using the item's order ids.
+    """
+
+    timestamp = datetime.now().timestamp()
+
+    conn = client.HTTPSConnection("market-proxy.gaijin.net")
+    payload = f"action=cancel_order&token={GM_TOKEN}&appid=1165&transactid={transact_id}&reqstamp={timestamp}&pairId={pair_id}&orderId={order_id}"
+    headers = {'accept': 'application/json, text/javascript, */*; q=0.01', 'accept-language': 'en-CA,en;q=0.8', 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    conn.request("POST", "/market", payload, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read())
+    conn.close()
+
+    if data["response"]["success"] != True:
+        return False
+    else:
+        return True
+
+def cancel_orders() -> bool:
+    """
+    Cancels all orders using each item's order ids.
+    """
+
+    pass
+
+def create_order() -> bool:
+    """
+    Creates an order for an item.
+    """
+
+    pass
 
 def get_item_variable_data(hash_name: str) -> tuple:
     """
@@ -66,7 +128,6 @@ def get_item_variable_data(hash_name: str) -> tuple:
     conn.request("POST", "/web", payload, headers)
     res = conn.getresponse()
     data = json.loads(res.read())
-
     conn.close()
 
     if (data['response']['success'] != True):
@@ -199,6 +260,15 @@ def get_items_inventory_data() -> dict:
     conn.close()
     return ids_mapping
 
+
+# Database functions
+def db_get_hash_by_id(asset_id: int):
+    """
+    Returns the item's hash_name by its argument asset_id from the database.
+    """
+    DB_CURSOR.execute(queries[3].format(asset_id))
+    return DB_CURSOR.fetchone()[0]
+
 def db_populate_items_static():
     static_data = get_items_static_data()
 
@@ -230,3 +300,5 @@ def db_populate_items_inventory():
 #?    (price_buy, price_sell, quantity_buy, quantity_sell, profit, roi, timestamp)
 #? 3. Add a tags table, link to asset_id from items_static
 #?    (should be populated along with db_populate_items_static simultanously)
+#? 4. Learn how to write formal python unit tests, and create one which does:
+#?    Opens an order for an item and cancels the order for the item (add delays and in-between checks)
