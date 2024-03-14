36f107229b60312ef0e7fd245387dce807695e28
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-03-14 13:28:51 -0400

    Update MVC architecture

--- DEPRECATEsrc/functions.py
+++ DEPRECATEsrc/functions.py
@@ -1,15 +1,13 @@
 # --- Imports
 from classes import Item  # class to preserve a market item
 from http import client  # used to make API requests
-from dotenv import load_dotenv  # used in loading .env file
 import sqlite3
 from datetime import datetime  # used to define the timestamp of requested GM items
 import json  # used to load settings from settings.json
 import os  # used in setting environment variables
 
 # --- Setup
-load_dotenv()  # load environment variables
-with open('../db/queries.sql', 'r') as file:  # read SQL querries
+with open('../db/queries.sql', 'r') as file:  # read SQL queries
   sql_content = file.read()
 queries = [query.split('\n', 1)[1].strip() for query in sql_content.split('-- #') if query.strip()]
 
@@ -21,96 +19,6 @@ cursor = conn.cursor()
 
 
 # --- General functions
-def get_balance() -> float:
-  """
-    Gets the balance of the user.
-    If successful, returns a float.
-    """
-
-  # Get data
-  conn = client.HTTPSConnection("wallet.gaijin.net")
-  headers = {'Authorization': f'BEARER {GM_TOKEN}'}
-  conn.request("GET", "/GetBalance", '', headers)
-  res = conn.getresponse()
-  data = json.loads(res.read())
-  conn.close()
-
-  # Check if status is good
-  if (data["status"] != "OK"):
-    print(f"ERROR: could not get balance.\nStatus: {data['status']}")
-    return -1
-  else:
-    return float(data["balance"]) / 10000
-
-
-def get_open_orders() -> list[tuple]:
-  """
-    Gets current open orders. Returns a list of tuples with data organized like so:
-    (transact_id, order_id, pair_id, hash_name, type, price, amount, timestamp).
-    """
-
-  conn = client.HTTPSConnection("market-proxy.gaijin.net")
-  payload = f"action=cln_get_user_open_orders&token={GM_TOKEN}&appid=1165"
-  headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-  conn.request("POST", "/web", payload, headers)
-  res = conn.getresponse()
-  data = json.loads(res.read())
-  conn.close()
-
-  open_orders = []
-
-  if (data['success'] != True):
-    print("ERROR: Could not make request for open orders.")
-    return []
-
-  for item in data['response']:
-    try:
-      open_orders.append((int(item['txId']), int(item['id']), int(item['pairId']), item['market'], item['type'],
-                          round(int(item['localPrice']) / 10000, 2), int(item['amount']), item['time']))
-    except:
-      print(f"ERROR: Could not get item in open orders with ID: {item['id']}")
-
-  return open_orders
-
-
-def cancel_order(transact_id: int, order_id: int, pair_id: int) -> bool:
-  """
-    Cancels an open order using the item's order ids.
-    """
-
-  timestamp = datetime.now().timestamp()
-
-  conn = client.HTTPSConnection("market-proxy.gaijin.net")
-  payload = f"action=cancel_order&token={GM_TOKEN}&appid=1165&transactid={transact_id}&reqstamp={timestamp}&pairId={pair_id}&orderId={order_id}"
-  headers = {'accept': 'application/json, text/javascript, */*; q=0.01', 'accept-language': 'en-CA,en;q=0.8',
-             'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-  conn.request("POST", "/market", payload, headers)
-  res = conn.getresponse()
-  data = json.loads(res.read())
-  conn.close()
-
-  if data["response"]["success"] != True:
-    return False
-  else:
-    return True
-
-
-def cancel_orders() -> bool:
-  """
-    Cancels all orders using each item's order ids.
-    """
-
-  pass
-
-
-def create_order() -> bool:
-  """
-    Creates an order for an item.
-    """
-
-  pass
-
-
 def get_item_variable_data(hash_name: str) -> tuple:
   """
     Gets variable data for a single item by its hash name.
@@ -295,12 +203,3 @@ def db_populate_items_inventory():
     cursor.execute(queries[2].format(i, json.dumps(ids_mapping[i]), len(ids_mapping[i])))
 
   conn.commit()
-
-# ? Next steps:
-# ? DONE - 1. Put static information into new database table (asset_id, name, hash_name)
-# ? DONE - 2. Link that table to another table containing variable data
-# ?    (price_buy, price_sell, quantity_buy, quantity_sell, profit, roi, timestamp)
-# ? 3. Add a tags table, link to asset_id from items_static
-# ?    (should be populated along with db_populate_items_static simultanously)
-# ? 4. Learn how to write formal python unit tests, and create one which does:
-# ?    Opens an order for an item and cancels the order for the item (add delays and in-between checks)
--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,7 @@
-from model import Item
 from view import View
+# from dotenv import load_dotenv
 
 class Controller:
   def __init__(self):
+    # load_dotenv()
     self.view = View()
