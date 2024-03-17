bdbd05e3a9a3881e01613728ba26c071b9b79a7f
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-03-17 13:43:27 -0400

    1st publish

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name="gmcli",
     version="0.0.1",
     description="CLI to interact with the Gaijin Market.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/JowiAoun/Gaijin-Market-CLI",
     author="Jowi Aoun",
     packages=find_packages(),
