56f2e4c96e7085f66fdce3cdfc13c35ada88bf92
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-07-02 01:07:48 -0400

    Update default vals

--- gmcli/models/Item.py
+++ gmcli/models/Item.py
@@ -5,14 +5,15 @@ class Item(BaseModel):
   """
   Class representing an item from the Gaijin market.
   """
-  asset_id: int
-  name: str
-  hash_name: str
-  price_buy: float
-  price_sell: float
-  quantity_buy: int
-  quantity_sell: int
-  tags: dict
+  cvalue: int = None
+  id: int = None  # Unique identifier for each Gaijin Market item
+  name: str = None
+  hash_name: str = None
+  price_buy: float = None
+  price_sell: float = None
+  quantity_buy: int = None
+  quantity_sell: int = None
+  tags: dict = {}
   timestamp: int = Field(default_factory=lambda: int(datetime.now().timestamp()))
 
   @property
@@ -21,3 +22,6 @@ class Item(BaseModel):
   @property
   def roi(self) -> int:
     return int((self.profit / self.price_buy) * 100)
+
+  def __str__(self):
+    return f"Item with id {self.id} and asset_id {self.cvalue}"
